a126c0adea6f596683e2c1e079f242266f2a10b5
Sanam <76154182+Sanam20302@users.noreply.github.com>
2025-01-19 21:59:17 +0530

    Update Sentiment_Analysis_App.py

--- Sentiment_Analysis_App.py
+++ Sentiment_Analysis_App.py
@@ -1,48 +1,53 @@
-import streamlit as st
 import pandas as pd
+import streamlit as st
 from textblob import TextBlob
-from io import StringIO
+import cleantext
 
-# Function to analyze sentiment
-def analyze_sentiment(quote):
-    analysis = TextBlob(quote)
-    if analysis.sentiment.polarity > 0:
-        return 'positive'
-    elif analysis.sentiment.polarity < 0:
-        return 'negative'
-    else:
-        return 'neutral'
+st.header("Sentiment Analysis")
 
-# Streamlit app
-def main():
-    st.title('Sentiment Analysis of Quotes')
+with st.expander("Analyse Text"):
+    text = st.text_input("Enter the text here")
+    if text:
+        data = TextBlob(text)
+        st.write("Polarity : ",round(data.sentiment.polarity,2))
+        st.write("Subjectivity : ",round(data.sentiment.subjectivity,2))
     
-    uploaded_file = st.file_uploader('Upload a CSV file', type=['csv'])
-    
-    if uploaded_file is not None:
-        # Read the uploaded CSV file
-        data = pd.read_csv(uploaded_file)
-        
-        # Check if the CSV file contains the necessary column
-        if 'quote' not in data.columns:
-            st.error('CSV file must contain a "quote" column')
+    text2 = st.text_input("Clean text")
+    if text2:
+        st.write(cleantext.clean(text2,clean_all=False,extra_spaces=True,stopwords=True,lowercase=True,numbers=True,punct=True))
+with st.expander("Analyse CSV"):
+    upl_file = st.file_uploader("Upload File")
+
+    def pol_score(text):
+        blob2 = TextBlob(text)
+        return blob2.polarity
+    def sentiment(score):
+        if score <= -0.5:
+            return "Negative"
+        elif score >= 0.5:
+            return "Positive"
         else:
-            # Analyze sentiment for each quote
-            data['sentiment'] = data['quote'].apply(analyze_sentiment)
-            
-            # Display the resulting dataframe
-            st.write(data)
-            
-            # Convert dataframe to CSV
-            output = StringIO()
-            data.to_csv(output, index=False)
-            processed_data = output.getvalue()
-            
-            # Provide download link for the new CSV file
-            st.download_button(label='Download CSV with Sentiments',
-                               data=processed_data,
-                               file_name='quotes_with_sentiments.csv',
-                               mime='text/csv')
+            return "Neutral"
+    def clean_txt(text):
+        cleaned_txt = cleantext.clean(str(text),clean_all=False,extra_spaces=True,stopwords=True,lowercase=True,numbers=True,punct=True)
+        return cleaned_txt
+    
+    if upl_file:
+        df = pd.read_csv(upl_file)
 
-if __name__ == '__main__':
-    main()
+        df['score'] = df['text'].apply(pol_score)
+        df['sentiment'] = df['score'].apply(sentiment)
+        st.write(df.head())
+
+    @st.cache_data
+    def convert_df(df):
+        return df.to_csv().encode('utf-8')
+    
+    final_csv = convert_df(df)
+    st.download_button(
+        label = "Download CSV",
+        data = final_csv,
+        file_name = 'after_senti_anaysis.csv',
+        mime = 'text/csv'
+        )
+
